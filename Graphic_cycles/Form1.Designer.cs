ec2f7519f56a90b461cb304885b46542de7e8f4a
Hoangsat <hoangsat1@gmail.com>
2022-07-11 22:40:32 +0100

    New buttons
    Hoanh Din, [11.07.2022 22:39]
    1. Added button 2 to draw horizontal lines.
      2. Added button 3 to draw vertical lines.
      3. Added button 4 to draw a grid.
      4. Added the Sleep1() method for a 100 ms delay.
      5. Added a delay inside each cycle.

--- Graphic_cycles/Form1.Designer.cs
+++ Graphic_cycles/Form1.Designer.cs
@@ -31,22 +31,25 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonClear = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 174);
+            this.pictureBox1.Location = new System.Drawing.Point(72, 83);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.Size = new System.Drawing.Size(301, 301);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // buttonClear
             // 
             this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonClear.Location = new System.Drawing.Point(463, 21);
+            this.buttonClear.Location = new System.Drawing.Point(334, 21);
             this.buttonClear.Name = "buttonClear";
             this.buttonClear.Size = new System.Drawing.Size(101, 36);
             this.buttonClear.TabIndex = 1;
@@ -65,11 +68,47 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(119, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(226, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(12, 401);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 36);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 486);
+            this.ClientSize = new System.Drawing.Size(463, 458);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.pictureBox1);
@@ -89,6 +128,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button buttonClear;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Graphic_cycles/Form1.cs
+++ Graphic_cycles/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -47,6 +48,46 @@ namespace Graphic_cycles
         {
             g.DrawLine(pen, 0, 0, 300, 300);
             g.DrawLine(pen, 300, 0, 0, 300);
+            RefreshPicture();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int y = 0; y <= 300; y += 30)
+            {
+                g.DrawLine(pen, 0, y, 300, y);
+                sleep1();
+            }
+            RefreshPicture();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            for (int x = 0; x <= 300; x += 30)
+            {
+                g.DrawLine(pen, x, 300, x, 0);
+                sleep1();
+            }
+            RefreshPicture();
+        }
+        public void sleep1()
+        {
+            RefreshPicture();
+            Thread.Sleep(100);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+                for (int y = 0; y <= 300; y += 30)
+            { 
+                    g.DrawLine(pen, 0, y, 300, y);
+                sleep1();
+                int x = y; 
+            
+                g.DrawLine(pen, x, 300, x, 0);
+                sleep1();
+            }
             RefreshPicture();
         }
     }
